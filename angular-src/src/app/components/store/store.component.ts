87d2833ae9edaa979d67ac0aa7ad0ee3e48d3138
amhenk <amhenkemeier@gmail.com>
2017-04-15 23:49:37 -0500

    Added first steps toward item lists

--- angular-src/src/app/components/store/store.component.ts
+++ angular-src/src/app/components/store/store.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  items: item[] = [];
+  chips: item;
+  apple: item;
 
   constructor() { }
 
   ngOnInit() {
+    this.chips = {
+      category : "Pantry",
+      name : "Doritios",
+      price : 1.99,
+      item_id: "1233AB"
+    };
+
+    this.apple = {
+      category : "Produce",
+      name : "Granny Smith Apple",
+      price : 1.49,
+      item_id: "94823AC"
+    };
+
+    this.items.push(this.chips);
+    this.items.push(this.apple);
   }
 
+}
+
+interface item{
+  category: string;
+  name: string;
+  price: number;
+  item_id: string;
 }
