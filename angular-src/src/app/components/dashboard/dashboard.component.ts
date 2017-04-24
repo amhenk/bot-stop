dccf66b50c13b7921e5de2b18c74d3d7887e7d30
amhenk <amhenkemeier@gmail.com>
2017-04-24 14:35:35 -0500

    Updated dashboard buttons and added user class so we can make updates if necessary

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  name: String;
+  username: String;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(profile =>{
+      this.user = profile.user;
+      this.name = this.user.name;
+      this.username = this.user.username;
+    }),
+    err => {
+      console.log(err);
+      return false;
+    }
   }
 
   startNewList(){
