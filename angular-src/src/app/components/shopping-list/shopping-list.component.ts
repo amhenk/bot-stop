71025936ddaad9c44d2bf0a9d02ce2841d93dd25
amhenk <amhenkemeier@gmail.com>
2017-05-13 13:28:01 -0500

    Can now remove items

--- angular-src/src/app/components/shopping-list/shopping-list.component.ts
+++ angular-src/src/app/components/shopping-list/shopping-list.component.ts
@@ -11,17 +11,20 @@ import { ItemService } from '../../services/item.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  items: String[] = [];
+  items: Item[] = [];
   item_name: String;
-  curr_item_list: Item[] = [];
-
+  curr_item_list: Item[];
+  current_item: Item;
 
   constructor(private itemService: ItemService,
               private flashMessage: FlashMessagesService
   ) { }
 
   ngOnInit() {
-    this.item_name = '';
+    this.item_name = 'Milk';
+    this.curr_item_list = [];
+    this.current_item = null;
+    this.getUserSelectedItem();
   }
 
   getUserSelectedItem() {
@@ -38,14 +41,33 @@ export class ShoppingListComponent implements OnInit {
       console.log('Oh no!');
       return false;
     });
-    this.item_name = '';
   }
 
   // TODO: Get Items from the current item list
-  addItemToList(){
-    console.log('Adding item: ' + this.item_name);
-    this.items.push(this.item_name);
-    this.item_name='';
+  addItemToList(item){
+    console.log('Adding item: ' + item);
+    this.items.push(item);
+    this.item_name = '';
+
+    // Empty the searched items
+    this.curr_item_list = [];
+  }
+
+  displayItemInfo(selected_item){
+    // Empty the item list to dispaly the selected item's info
+    this.curr_item_list = [];
+
+    this.current_item = selected_item;
+    console.log('Selected Item: ' + this.current_item.name);
+  }
+
+  removeSelectedItem() {
+    for(var i = this.items.length - 1; i >= 0; i--) {
+      if(this.items[i] === this.current_item){
+        this.items.splice(i,1);
+        this.current_item = null;
+      }
+    }
   }
 
 }
